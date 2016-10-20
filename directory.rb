2142892d0aa5e23e16890103a6cf41243fe34a7f
skyler <Smoreley@gmail.com>
2016-10-20 10:54:17 -0400

    delete_all

--- directory.rb
+++ directory.rb
@@ -7,6 +7,10 @@ get '/display' do
     erb :display_tables; #, :layout => :profile
 end
 
+get '/feed' do
+    erb :postfeed; #, :layout => :profile
+end
+
 get('/entrance'){ File.read(File.join('public', 'html/entrance.html')) }
 
 get('/register'){ File.read(File.join('public', 'html/register.html')) }
@@ -17,22 +21,27 @@ get('/account'){ File.read(File.join('public', 'html/account.html')) }
 
 get('/posts'){ File.read(File.join('public', 'html/posts.html')) }
 
-get('/delete'){ 
+post('/delete'){ 
     User.find(session[:id]).destroy
     File.read(File.join('public', 'html/delete.html')) 
 }
 
-get '/:name' do
-    erb :error404
-    
-    case params[:name]
-    when "home"
-        erb :error404;
-        
-    when "login"
-        erb :error404;        
-    else
-        erb :error404;
-    end
-    
-end
+post('/delete_all'){
+    User.delete_all()
+}
+
+
+#get '/:name' do
+#    erb :error404
+#    
+#    case params[:name]
+#    when "home"
+#        erb :error404;
+#        
+#    when "login"
+#        erb :error404;        
+#    else
+#        erb :error404;
+#    end
+#    
+#end
